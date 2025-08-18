d7ade8efce57077bb071a41f7fcc492256a64a07
magley <60552482+magley@users.noreply.github.com>
2025-08-18 14:29:09 +0200

    feat: [Just testing if minor version will bump]

--- app/main.go
+++ app/main.go
@@ -16,7 +16,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello docker!!!")
+		return c.HTML(http.StatusOK, "Hello docker!!!!")
 	})
 
 	e.GET("/health", func(c echo.Context) error {
